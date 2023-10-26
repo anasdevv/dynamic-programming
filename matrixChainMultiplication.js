c2c67f0b1376e5107747a60a395add8af6eca543
Anas <anas1.dev@gmail.com>
2023-10-26 15:43:00 +0500

    memoized matrix chain multiplication

--- matrixChainMultiplication.js
+++ matrixChainMultiplication.js
@@ -1,8 +1,11 @@
 const { MAX_UNSIGNED_VALUE } = require('long');
 
-const matrixChainMultiplication = (dims) => {
+const matrixChainMultiplication = (dims, memo = new Map()) => {
   if (dims.length <= 2) {
     return 0;
+  }
+  if (memo.has(dims)) {
+    return memo.get(dims);
   }
   let minCost = MAX_UNSIGNED_VALUE;
   for (let i = 1; i < dims.length; i++) {
@@ -10,8 +13,13 @@ const matrixChainMultiplication = (dims) => {
       matrixChainMultiplication(dims.slice(0, i)) +
       matrixChainMultiplication(dims.slice(i)) +
       dims[0] * dims[i] * dims[dims.length - 1];
-    if (cost < minCost) minCost = cost;
+    if (cost < minCost) {
+      minCost = cost;
+      memo.set(dims, minCost);
+    }
   }
-  return minCost;
+  return memo.get(dims);
 };
+30, 35, 15, 5, 10, 20, 25;
 console.log(matrixChainMultiplication([6, 2, 5, 20]));
+console.log(matrixChainMultiplication([30, 35, 15, 5, 10, 20, 25]));
