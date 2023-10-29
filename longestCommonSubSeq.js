69bb16b137547cb059ead25d87b78f225d1000cf
Anas <anas1.dev@gmail.com>
2023-10-29 15:48:30 +0500

    LCS small bug fix :

--- longestCommonSubSeq.js
+++ longestCommonSubSeq.js
@@ -3,16 +3,17 @@ const LCS = (str1, str2) => {
     if (`${i}${j}` in memo) return memo[`${i}${j}`];
     if (i === 0 || j === 0) return 0;
     if (str1[i] === str2[j]) {
-      return (memo[`${i}${j}`] = 1 + func(i - 1, j - 1, memo));
+      memo[`${i}${j}`] = 1 + func(i - 1, j - 1, memo);
+      return memo[`${i}${j}`];
     } else {
-      return (memo[`${i}${j}`] = Math.max(
-        func(i - 1, j, memo),
-        func(i, j - 1, memo)
-      ));
+      memo[`${i}${j}`] = Math.max(func(i - 1, j, memo), func(i, j - 1, memo));
+      return memo[`${i}${j}`];
     }
   };
-  return func(str1.length - 1, str2.length - 1);
+  return func(str1.length, str2.length);
 };
 console.log(LCS('ABCDGH', 'AEDFHR'));
 console.log(LCS('AGGTAB', 'GXTXAYB'));
+console.log(LCS('XMJYAUZ', 'MZJAWXU'));
+console.log(LCS('ABCBDAB', 'AEDFHR'));
 ``;
